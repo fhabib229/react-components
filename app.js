3a35ec3504472838f8fb693c17385a49008b3bc4
Faris Habib <faris.habib.7@gmail.com>
2018-09-29 00:30:13 -0700

    Complete bolding GroceryListItem on hover

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 class GroceryListItem extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
+    this.state = {
+      hover: false
+    };
+  }
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
   }
   render() {
-  return (
-    <li>{this.props.item}</li>
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
     );
+
   }
+
 }
 
 var GroceryList = (props) => (
   <ul>
     {props.items.map(item =>
-      <GroceryListItem item={item} />
+      <GroceryListItem key={item} item={item} />
     )}
   </ul>
 );
