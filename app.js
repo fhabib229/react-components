eeec690d802214ecaf6dec8b53429473e0d0f20c
Faris Habib <faris.habib.7@gmail.com>
2018-09-28 23:14:46 -0700

    Made grocery list of 2 items

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// TODO
+const groceryList = ['Chicken', 'Spinach'];
+const listItems = groceryList.map((item) =>
+  <li>{item}</li>
+);
+ReactDOM.render(
+  <ul>{listItems}</ul>,
+  document.getElementById('app')
+);
