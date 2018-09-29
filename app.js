5cb0077f2230e466b97692409e031ba8ee823001
Faris Habib <faris.habib.7@gmail.com>
2018-09-28 23:30:36 -0700

    Complete adding 2 nested item components

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const groceryList = ['Chicken', 'Spinach'];
-const listItems = groceryList.map((item) =>
-  <li>{item}</li>
-);
-ReactDOM.render(
-  <ul>{listItems}</ul>,
-  document.getElementById('app')
+function GroceryList() {
+  return (
+    <ul>
+    <GroceryItem name="Chicken" />
+    <GroceryItem name="Spinach" />
+    </ul>
+  );
+}
+function GroceryItem(props) {
+  return <li>{props.name}</li>;
+}
+// function Chicken() {
+//   return <li>Chicken</li>;
+// }
+// function Spinach() {
+//   return <li>Spinach</li>;
+// }
+ReactDOM.render(<GroceryList/>, document.getElementById('app')
 );
